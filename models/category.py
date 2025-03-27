a15c064585381f0a3d3a79b3adb88f30128983b2
Anatoli <PanasAA@web.de>
2025-03-27 20:14:37 +0100

    add all

--- models/category.py
+++ models/category.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from models import db
-# from models.questions import Question
+from models.questions import Question
 
 
 class Category(db.Model):
